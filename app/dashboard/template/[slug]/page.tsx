eced638a477253f88fccda1b0b092d6bab05d19a
Mark.Zhang <mingxiangzhang_mark@outlook.com>
2025-01-03 16:34:54 +1100

    search

--- app/dashboard/template/[slug]/page.tsx
+++ app/dashboard/template/[slug]/page.tsx
@@ -148,13 +148,17 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import template from "@/utils/template";
 import { Button } from '@/components/ui/button';
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft, CircleCheck, Copy, Loader2Icon } from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { useParams } from 'next/navigation';
+import { runAi } from '@/app/actions/ai';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import toast from "react-hot-toast";
 
 export interface Template {
   name: string;
@@ -175,32 +179,82 @@ export interface Template {
  export default function page() {
   const params = useParams(); // 使用 useParams 解包参数
   const slug = params.slug as string; // 解包 slug
+  const [query, setQuery] = useState< string > ("");
+  const [content, setContent] = useState < string > ("") ;
+  const [loading, setLoading] = useState < boolean > (false);
+  const editorRef = React.useRef<any>(null);
+  const [copySuccess, setCopySuccess] = useState(false);
+
+    React.useEffect(() => {
+    if (content) {
+      const editorInstance = editorRef.current.getInstance();
+      editorInstance.setMarkdown(content);
+    }
+  }, [content]);
+
   const t = template.find((item) => item.slug === params.slug) as
  Template;
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  console.log("submitted");
+ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    // console.log("submitted => ", t.aiPrompt, query);
+    try {
+    const data = await runAi(t.aiPrompt + query);
+    setContent(data);
+    // save to db (userEmail, query, content, templateSlug)
+    } catch (err) {
+    setContent("Failed to generate content. Try again.");
+    } finally {
+    setLoading(false);
+    }
+ };
+
+   const handleCopy = async () => {
+    const editorInstance = editorRef.current.getInstance();
+    const c = editorInstance.getMarkdown(); // .getHTML()
+
+    try {
+      await navigator.clipboard.writeText(c);
+      toast.success("Content copied to clipboard.");
+      setCopySuccess(true);
+      setTimeout(() => setCopySuccess(false), 3000); 
+    } catch (err) {
+      toast.error("An error occurred. Please try again.");
+      setCopySuccess(false);
+    }
+    
+    // setCopySuccess(false);
   };
-  
-  const handleChange = (
-  e:
-  | React.ChangeEvent<HTMLInputElement>
-  | React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-  e.preventDefault();
-  console.log(e.target.value);
-  };
+
+ 
 
   return (
     <div className="mt-3">
-      <Link href="/dashboard" className="ml-5 ">
-        <Button>
-          <ArrowLeft />
-          Back
+       <div className="flex justify-between mx-3 my-3">
+       <Link href="/dashboard">
+          <Button>
+           <ArrowLeft /> <span className="ml-2">Back</span>
+         </Button>
+       </Link>
+
+        <Button  onClick={handleCopy}>
+          {/* <Copy /> <span className="ml-2">Copy</span> */}
+          {copySuccess ? (
+              < >
+                <CircleCheck className="mr-0 " />
+                <span>Copied</span>
+              </>
+            ) : (
+              <>
+                <Copy className="mr-2" />
+                <span>Copy</span>
+              </>
+            )}
         </Button>
-      </Link>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 p-5">
+      </div>
+      
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 pb-5 px-3">
         <div className="col-span-1 bg-slate-100 dark:bg-slate-900 rounded-md border p-5">
           <div className="flex flex-col gap-2">
           <Image src={t.icon} alt={t.name} width={50} height={50} />
@@ -216,24 +270,37 @@ export interface Template {
 
                 name={item.name}
                 required={item.required}
-                onChange={handleChange}
+                onChange={(e) => setQuery(e.target.value)}
               />
             ) : (
                 <Textarea
                 
                 name={item.name}
                 required={item.required}
-               onChange={handleChange}
+                onChange={(e) => setQuery(e.target.value)}
               />
             )}
             </div>
             ))}
-            <Button type="submit" className="w-full py-6">
-               Generate content
+           <Button type="submit" className="w-full py-6" disabled={loading}>
+            {loading && <Loader2Icon className="animate-spin mr-2" />} Generate
+            content
             </Button>
           </form>
         </div>
-          <div className="col-span-2">generated content</div>
+          <div className="col-span-2">
+                    <Editor
+            ref={editorRef}
+            initialValue="Generated content will appear here."
+            previewStyle="vertical"
+            height="600px"
+            initialEditType="wysiwyg"
+            useCommandShortcut={true}
+            onChange={() =>
+              setContent(editorRef.current.getInstance().getMarkdown())
+            }
+          />
+          </div>
         </div>
         </div>
         );
